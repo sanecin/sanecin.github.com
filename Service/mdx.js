438175b1cad831200336354f7d30506ed3279c56
SanEcIn <sanecin@gmail.com>
2014-12-30 05:16:28 +0300

    .

---
+++ Service/mdx.js
@@ -0,0 +1,14 @@
+//[¤[mdx][https://mdx.googlecode.com/git/Mdx.js]@[vers_1.0_¤]¤¤¤ ...
+
+//(function (){
+
+// 	'use strict';
+
+	var mdx='¤';
+	window[mdx]="mdx¤";
+
+//	Event.On(window,"load",function (){Id.mdx.innerHTML=window[mdx];});
+
+//}());
+
+//<!-- /* [Copyright 11/12/2012 SanEcIn][¤¤] */-->
