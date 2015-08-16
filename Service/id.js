c1cd84827cb7789497147f1d8dcaed612399f714
SanEcIn <sanecin@gmail.com>
2015-08-16 17:11:31 +0300

    ID query

--- Service/id.js
+++ Service/id.js
@@ -1,25 +1,15 @@
-//[¤[mdx][https://mdx.googlecode.com/git/id.js][https://github.sanecin.ru/code/js/id.js]@[vers_1.0_¤]¤¤¤ ...
+//[¤[mdx][https://mdx.googlecode.com/git/code/js/id.js][https://raw.githubusercontent.com/sanecin/code/gh-pages/js/id.js][http://sanecin.ru/code/js/id.js]@[vers_1.0_¤]¤¤¤ ...
 
 //======== Id ===========
-// ... element ID query ... speed ... element[ID="f"] => Id.f ... !q ...
-
-(function(){
-
- 	'use strict';
-
-	var Id=window.Id={};
-
-	var i,j;
-// ... for next ... window.onload only ...
-	var _=function (){for(i=(j=document.querySelectorAll('[id]')).length;i--;Id[j[i].id]=j[i]){}}
-
-// ... toGO ... common
-	if (window.addEventListener) window.addEventListener("load",_, false); else window.onload=_;
-
 // ... in case ... event.js REQUIRED ...
-//	if (typeof(Event.On)==="function") ... ???
-//		Event.On(window,"load",Fit._);
+// ... element ID query ... speed  !q?w ... ... element[ID="f"] => Id.f ...
 
-})();
+	var Id={
+
+		_:function(){for(i=(j=document.querySelectorAll('[id]')).length;i--;Id[j[i].id]=j[i]){}}
+
+	};
+
+	Event.On(window,"load",Id._);
 
 //<!-- /* [Copyright 11/12/2012 SanEcIn][¤¤] */-->
